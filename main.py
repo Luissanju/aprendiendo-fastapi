fd5e170948de9ce2413c61900cb42e8ef9be2d27
Luis Sanjuan Diaz <luissanju25@gmail.com>
2026-09-21 10:50:54 +0200

    feat: primera API con FastAPI

---
+++ main.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+usuarios = [
+    {"id": 1, "nombre": "Luis", "edad": 25},
+    {"id": 2, "nombre": "Ana", "edad": 22},
+    {"id": 3, "nombre": "Carlos", "edad": 30}
+]
+
+
+@app.get("/")
+def inicio():
+    return {"mensaje": "Mi primera API"}
+
+
+@app.get("/usuarios")
+def obtener_usuarios():
+    return usuarios
+
+
+@app.get("/usuarios/{usuario_id}")
+def obtener_usuario(usuario_id: int):
+
+    for usuario in usuarios:
+        if usuario["id"] == usuario_id:
+            return usuario
+
+    return {"error": "Usuario no encontrado"}
+
+
+@app.get("/saludo/{nombre}")
+def saludo(nombre: str):
+    return {"mensaje": f"Hola, {nombre}"}
